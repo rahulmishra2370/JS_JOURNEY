79d3d43e067f35c13ddfa95f00afd7562ca988da
RAHUL MISHRA <rahulmishra2370@gmail.com>
2024-10-10 14:11:47 +0530

    operators post and pre increment

---
+++ strng_to_numbers.js
@@ -0,0 +1,25 @@
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// let strn1 = "rahul";
+// let strn2 = "mishra";
+// let strn3 = strn1 + " " + strn2;
+// console.log(strn3);
+
+// pre increment
+let x = 1;
+let y = x++;
+console.log(x);
+console.log(y);
+
+// x ke case mei kya hua ki post increment hai iska mtlab ye hai ki value increment hogi but return value purani hogi
+// jaise x ko humne increment kr dia jab usko assign kia tha y ke andar toh actually x toh increment hogya but y mei assign hote time jab y ko return kia toh usme purani value ayi
+
+//pre increment
+let a = 1;
+let b = ++a;
+console.log(a);
+console.log(b);
+// pre increment ke case mei value increment bhi hogi aur return value bhi increment wali hi ane wali hai no previous value as of in the case of post increment
