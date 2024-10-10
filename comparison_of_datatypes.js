871bd31b48d150e60324a52821d032e74630c862
RAHUL MISHRA <rahulmishra2370@gmail.com>
2024-10-10 14:40:18 +0530

    comparison_in_js

---
+++ comparison_of_datatypes.js
@@ -0,0 +1,13 @@
+// console.log("1" > 0);
+// console.log(1 > "0");
+// here what compariso operator did is that it automatically convert the value of string into number and then start the compariosn
+
+// console.log(null=0)  bhai ye cheeezz not allowed in js
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null <= 0);
+// here what happend is that the value of null is empty means there is nothing but a memory created in the computer for this ek dabba ban gya hai
+// jo ki khai hia toh uss khali cheez ka zero se bada hona aur chota hona makes no sense means false
+// but uss khali cheez ka zero se equal ya bada hona kuch sense create krta hai isilye true
