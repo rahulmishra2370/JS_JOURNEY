c18bfcc63a290d90e21230e454a0aab2193ee2d5
RAHUL MISHRA <rahulmishra2370@gmail.com>
2024-10-11 14:41:02 +0530

    date and time in js

---
+++ date_and_time.js
@@ -0,0 +1,14 @@
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleDateString());
+console.log(date.toISOString());
+console.log(date.toDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.toTimeString());
+
+//+++++++++++++++++++++++++++++++++++++++
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.toDateString(date.getDay()));
+console.log(date.getHours());
