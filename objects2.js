5e9d94e7eaa6985cf5447e9e6bfb969f2ed307a4
RAHUL MISHRA <rahulmishra2370@gmail.com>
2024-10-17 21:45:59 +0530

    did nothing today

--- objects2.js
+++ objects2.js
@@ -1,1 +1,12 @@
 /// here we learn more about the objects in the javascript
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+// Accessing the method
+console.log(person.fullName()); // Output: John Doe
